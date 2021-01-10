bcab2cd3dc15096f361545716de8924e6682e215
Stephanie Quon <stephanie.quon@alumni.ubc.ca>
2021-01-09 21:16:45 -0800

    logic for happiness counter

--- nwHacks2021/ViewController.swift
+++ nwHacks2021/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 import ImageIO
 
 class ViewController: UIViewController {
+    var counter = 0
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        counter += 1
+        counterLabel.text = "\(counter)"
+    }
     
     @IBOutlet var dogNeutralImage : UIImageView!
 

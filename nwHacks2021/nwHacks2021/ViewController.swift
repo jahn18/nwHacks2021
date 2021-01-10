2c2fd2f547c16a784791e9baf05e8211b041de0b
John Ahn <john.ahn.es@gmail.com>
2021-01-09 17:49:15 -0800

    Added base skeleton

---
+++ nwHacks2021/nwHacks2021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nwHacks2021
+//
+//  Created by joeahn@cisco.com on 2021-01-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ nwHacks2021/nwHacks2021Tests/nwHacks2021Tests.swift
@@ -0,0 +1,33 @@
+//
+//  nwHacks2021Tests.swift
+//  nwHacks2021Tests
+//
+//  Created by joeahn@cisco.com on 2021-01-09.
+//
+
+import XCTest
+@testable import nwHacks2021
+
+class nwHacks2021Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

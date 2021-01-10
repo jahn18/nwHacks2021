86a3814d5b911e01711eb0e434c5315dc7f2e54e
John Ahn <john.ahn.es@gmail.com>
2021-01-10 01:45:42 -0800

    Implemented speech detection

--- nwHacks2021/ViewController.swift
+++ nwHacks2021/ViewController.swift
@@ -8,34 +8,73 @@
 import Foundation
 import UIKit
 import ImageIO
+import Speech
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SFSpeechRecognizerDelegate {
     var counter = 0
     var runCount = 0
     
     @IBOutlet weak var counterLabel: UILabel!
     @IBOutlet weak var happinessLabel: UILabel!
-    //@IBAction func buttonTapped(_ sender: Any) {
-    //    counter += 1
-    //    counterLabel.text = "\(counter)"
-    //}
-    
     @IBOutlet var dogNeutralImage : UIImageView!
+            
+    @IBOutlet weak var detectedTextLabel: UILabel!
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+ 
+    /* Used for the audio */
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    
+    func recordAndRecognitionSpeech() {
+        //guard
+        let node = audioEngine.inputNode //else { return }
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in
+            self.request.append(buffer)
+        }
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            return print(error)
+        }
+        
+        guard let myRecognizer = SFSpeechRecognizer() else {
+            return
+        }
+        if !myRecognizer.isAvailable {
+            return
+        }
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: { result, error in
+            if let result = result {
+                let voiceRecognitionString = result.bestTranscription.formattedString
+                print("\(voiceRecognitionString)")
+            } else if let error = error {
+                print(error)
+            }
+        })
+        
+    }
+    
+    
+    @IBAction public func startButtonTapped(_ sender: UIButton) {
+        self.recordAndRecognitionSpeech()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        startButtonTapped(startButton)
+        /* Used for gestures */
         dogNeutralImage.loadGif(name: "Shiba-inu-taiki-2")
-        //dogNeutralImage.isHidden = true
-        
-        //let myView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
-        //myView.backgroundColor = .red
-        //myView.center = view.center
-        //view.addSubview(myView)
         
         Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
                     self.runCount += 1
-                    //print("Number: \(self.runCount)")
+                    print("Number: \(self.runCount)")
 
                     if self.runCount >= 2 {
                         self.dogNeutralImage.loadGif(name: "Shiba-inu-taiki-2")
@@ -80,6 +119,7 @@ class ViewController: UIViewController {
     }
     
     override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        dogNeutralImage.loadGif(name: "Shiba-inu-taiki")
         print("Device was shaken!")
     }
 
